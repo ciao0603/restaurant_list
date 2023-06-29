ecc947c12134a5fa07e42b6972c13402ef9d5da5
ciao0603 <jocelyn94032.0@gmail.com>
2023-06-29 10:52:33 +0800

    Introduction page

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const exphbs = require('express-handlebars')
 const port = 3001
 const restaurantList = require('./restaurant.json')
 
-app.engine('handlebars', exphbs({defaultLayout:'main'}))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurants : restaurantList.results })
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant })
 })
 
 app.listen(port, () => {
